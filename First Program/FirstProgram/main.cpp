e73457921bb384d29e52b959253f9e6ae9ac95e2
Asad ur Rehman <asadrehman515@gmail.com>
2024-10-17 21:19:38 +0500

    First Cpp Program

---
+++ First Program/FirstProgram/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int favorite_number;
+    std::cout << "Enter your Favorite Number: " << std::endl;
+    std::cin >> favorite_number;
+    std::cout <<"Same " << favorite_number <<" is my fav number too";
+    return 0;
+}
